3f93f7c918d9b01dd27412b929588404ec346f24
Scott <scott@winberg.us>
2018-04-15 12:48:43 -0600

    Big file changes

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+postCSS = require('gulp-postcss'),
+autoPrefixer = require('autoprefixer'),
+cssVars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postCSS([cssImport, cssVars, nested, autoPrefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
